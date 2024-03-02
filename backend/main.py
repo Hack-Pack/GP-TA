313809d47109f00f477822c217a22e38dde2a739
Bassim E <bassime@Bassims-MacBook-Air.local>
2024-03-02 15:20:37 -0800

    added cot reasoning

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,12 @@ def process_images(student_img_path, instructor_img_path, prompt_path, csv_path)
 
 
 def evaluate_question(question, student_answer, instructor_answer):
-    model = TextModel()
-    prompt = (f"Given the question: '{question}', evaluate the student's answer: '{student_answer}' "
-              f"against the instructor's answer: '{instructor_answer}'. Provide a detailed analysis.")
+    gpt3_model = TextModel(model="gpt-3.5-turbo-0125")
+    router_prompt = (f"Given the question: '{question}', evaluate if the student's answer: '{student_answer}' "
+              f"is 100% correct against the instructor's answer: '{instructor_answer}'. "
+              f"Return '(correct:1)' if the student's answer is fully correct, or '(correct:0)' if the answer is incorrect or if you are unsure.")
 
-    evaluation = model.complete(prompt=prompt, role="user")
-    return evaluation
+    first_evaluation = gpt3_model.complete(prompt=router_prompt, role="user")
+    eval_result = eval(first_evaluation.text.strip())
+
+    return eval_result
