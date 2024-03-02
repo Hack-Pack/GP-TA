4e60828f0e17089fbf10d75c2ab1c0c7e4991989
pavan3008 <pavansai3008@gmail.com>
2024-03-01 22:32:21 -0800

    commented code

--- backend/utils.py
+++ backend/utils.py
@@ -53,40 +53,23 @@ def process_form(prompt_path, img_path):
         return response
     except Exception as e:
         print(f"An error occurred: {e}")
-    
-# vision_model = VisionModel()
-# prompt = """I'll provide you images that has math questions and their respective answers, format them into a JSON object where each question is a key and its corresponding answer is the value.
-#             Ensure that each key-value pair is correctly formatted according to JSON standards.
-#             Note: The answers should contain each and every step, do not skip anything.
-#             Note: Do not add any information of answers other than what is written/present.
-            
-#             Here is the example format on how your response should be shown as below.
-#             Note: Replace the values in <> by the appropriate string values from the context.
-#             The format:
-#             {
-#             <Question 1>: <Complete Solution 1>,
-#             <Question 2>: <Complete Solution 2>,
-#             <Question 3>: <Complete Solution 3>,
-#             <Question 4>: <Complete Solution 4>,            
-#             }
-#         """
 
 # Instructor
-img_path = "/Users/pavan/Desktop/Hackathon/GP-TA/data/instructor_images"
-out_path = "/Users/pavan/Desktop/Hackathon/GP-TA/data/instructor.json"
-prompt_path = "/Users/pavan/Desktop/Hackathon/GP-TA/prompts/csv_promt.txt"
-csv_output = "/Users/pavan/Desktop/Hackathon/GP-TA/output.csv"
+# img_path = "data/instructor_images"
+# out_path = "data/instructor.json"
+# prompt_path = "prompts/csv_promt.txt"
+# csv_output = "output.csv"
 
-response = process_form(prompt_path, img_path)
-print(response)
+# response = process_form(prompt_path, img_path)
+# print(response)
 
-json_to_csv(response, csv_output)
+# json_to_csv(response, csv_output)
 
-# Student
-img_path = "/Users/pavan/Desktop/Hackathon/GP-TA/data/student_images"
-out_path = "/Users/pavan/Desktop/Hackathon/GP-TA/data/student.json"
+# # Student
+# img_path = "data/student_images"
+# out_path = "data/student.json"
 
-response = process_form(prompt_path, img_path)
-print(response)
+# response = process_form(prompt_path, img_path)
+# print(response)
 
-add_answers_to_csv(response, csv_output)
+# add_answers_to_csv(response, csv_output)
